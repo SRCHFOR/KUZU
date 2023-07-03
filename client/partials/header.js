a35d10273ebd3a3b2f4a648ec59c76b856547ce5
Matthew <Matthew@DESKTOP-SFAKSA4>
2023-07-02 23:41:14 -0500

    Session disconnect/reconnect

--- client/partials/header.js
+++ client/partials/header.js
@@ -1,11 +1,60 @@
 Template.HomeLayout.onCreated(function() {
   this.autorun(() => {
     this.subscribe('new-messages-count-user')
+
+	//*************************************************************************
+	//                 session disconnect/reconnect
+	//*************************************************************************
+	let disconnectTimer = null
+
+	// TODO update from https://github.com/mixmaxhq/meteor-smart-disconnect/blob/master/disconnect-when-backgrounded.js for Cordova
+
+	// 60 seconds by default
+	const disconnectTime = 60 * 1000
+
+	const removeDisconnectTimeout = () => {
+  		if (disconnectTimer) {
+    		clearTimeout(disconnectTimer)
+  		}
+	}
+
+	const createDisconnectTimeout = () => {
+  		removeDisconnectTimeout()
+
+  		disconnectTimer = setTimeout(() => {
+    		Meteor.disconnect()
+  		}, disconnectTime)
+	}
+
+	const disconnectIfHidden = () => {
+  		removeDisconnectTimeout()
+
+  		if (document.hidden) {
+    		createDisconnectTimeout()
+  		} else {
+    		Meteor.reconnect()
+  		}
+	}
+
+	document.addEventListener('visibilitychange', disconnectIfHidden)
+
+	Meteor.startup(disconnectIfHidden)
+
+	//console.log(`${new Date().toLocaleTimeString('en-GB')} : ${Meteor.status().status}`);
+	//*************************************************************************
   })
 })
 
 Template.HomeLayout.helpers({
-  currentActiveShow() {
-    return Shows.findOne({ isActive: true })
-  },
+  	currentActiveShow() {
+    	return Shows.findOne({ isActive: true })
+  	},
 })
+
+Template.HomeLayout.events({
+	'click .logout': function(event){
+		console.log('hi')
+		event.preventDefault();
+		AccountsTemplates.logout();
+	},
+});
